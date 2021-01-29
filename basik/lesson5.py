ebb3bb8175fc11947f80aa86410f85134926a6a1
Tomas Wender <tomas_wender@live.ru>
2021-01-29 22:16:44 +0600

    выполнено 6 из 7

--- basik/lesson5.py
+++ basik/lesson5.py
@@ -1,14 +1,40 @@
-
+import re
 
 
 
 #1. Создать программно файл в текстовом формате, записать в него построчно данные, вводимые пользователем. 
 # Об окончании ввода данных свидетельствует пустая строка.
 
+def create_file(way="folder_lesson5\my_file.txt"):
+    while True:
+        message = input('Введите текст: ')   
+        if message in '':
+            break
+        else:
+            my_file = open(way, 'a')
+            my_file.write(message+'\n')
+            my_file.close()
+
+
+#create_file()
 
 
 
 #2. Создать текстовый файл (не программно), сохранить в нем несколько строк, выполнить подсчет количества строк, количества слов в каждой строке.
+
+def count_line():
+    way = "folder_lesson5\my_file2.txt"
+    my_file = open(way, 'r')
+    content = my_file.readlines()
+    print(f'Колличество строк: {len(content)}')
+
+    for i in range(0, len(content)):
+        my_set = content[i].split(' ')
+        print(f'В строке {i+1}: {len(my_set)} слов')
+
+    my_file.close()
+
+#count_line()
 
 
 
@@ -16,8 +42,20 @@
 #3. Создать текстовый файл (не программно), построчно записать фамилии сотрудников и величину их окладов. 
 # Определить, кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников. Выполнить подсчет средней величины дохода сотрудников.
 
+def many():
+    way = "folder_lesson5\my_file3.txt"
+    my_file = open(way, 'r')
+    content = my_file.readlines()
+    szp = 0
+    print(content)
+    for i in range(0, len(content)):
+        my_set = content[i].split(' ')
+        if int(my_set[1]) < 20000:
+            print(f'Сотруник {my_set[0]} имеет ЗП меньше 20000')
+        szp +=  int(my_set[1])
+    print(f'Средняя ЗП сотрудников: {szp/len(content)}')
 
-
+#many()
 
 #4. Создать (не программно) текстовый файл со следующим содержимым:
 #One — 1
@@ -27,14 +65,51 @@
 #Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные. 
 # При этом английские числительные должны заменяться на русские. Новый блок строк должен записываться в новый текстовый файл.
 
+def trans():
 
+    transete = {
+      'One' : 'Один',
+      'Two' : 'Два',
+      'Three' : 'Три',
+      'Four' : 'Четыре'
+    }
+
+    way = "folder_lesson5\my_file4.txt"
+    my_file = open(way, 'r', encoding = 'utf-8')
+    content = my_file.readlines()
+    my_list = []
+    for i in range(0, len(content)):
+        my_set = content[i].split(' — ')
+        key = my_set[0]
+        my_list.append(f'{transete[key]} — {my_set[1]}')
+    my_file.close()
+    for elem in my_list:
+        my_file = open("folder_lesson5\my_file4_2.txt", 'a', encoding = 'utf-8')
+        my_file.write(elem)
+        my_file.close()
+
+
+#trans()
 
 
 #5. Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами. 
 # Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 
+def sumator():
+    way="folder_lesson5\my_file5.txt"
+    message = input('Введите числа через пробел: ')
+    my_file = open(way, 'a')
+    my_file.write(message+'\n')
+    my_file.close()
+    summ = 0
+    my_file = open(way, 'r', encoding = 'utf-8')
+    content = my_file.readlines()
+    my_set = content[0].split(' ')
+    for i in range(0, len(my_set)):
+        summ += int(my_set[i])
+    print(f'Сумма введенных чисел: {summ}')
 
-
+# sumator()
 
 #6. Необходимо создать (не программно) текстовый файл, где каждая строка описывает учебный предмет и наличие лекционных, 
 # практических и лабораторных занятий по этому предмету и их количество. Важно, чтобы для каждого предмета не обязательно были все типы занятий. 
@@ -48,8 +123,22 @@
 #{“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}
 
 
+def school():
+    way="folder_lesson5\my_file6.txt"
+    my_file = open(way, 'r', encoding = 'utf-8')
+    content = my_file.readlines()
+    my_dict = {
+    }
+    for con in content:
+        my_set = con.split(':')
+        match = re.findall(r'\d+', con)
+        summ = 0
+        for i in match:
+            summ += int(i)
+        keys = my_set[0]
+        my_dict[keys] = summ       
 
-
+# school()
 
 #7. Создать (не программно) текстовый файл, в котором каждая строка должна содержать данные о фирме: название, форма собственности, выручка, издержки.
 #Пример строки файла: firm_1 ООО 10000 5000.
